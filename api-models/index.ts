28ca86305cc647d7f0681c370127e786eaff5128
Ladislav Prix <ladislav.prix@designeo.cz>
2021-12-15 10:14:08 +0100

    Prettier rules in api-models

--- api-models/index.ts
+++ api-models/index.ts
@@ -1,53 +1,53 @@
 export type GetStatusByUsernameParams = {
-  username: string;
-};
+  username: string
+}
 
 export type GetStatusByEmailParams = {
-  email: string;
-};
+  email: string
+}
 
 export type CheckSessionResponseDto = {
-  hasSession: boolean;
-};
+  hasSession: boolean
+}
 
 export type CurrentUserResponseDto = {
-  id: string;
-  username: string;
-  email?: string | null;
-  isAnonymous: boolean;
-};
+  id: string
+  username: string
+  email?: string | null
+  isAnonymous: boolean
+}
 
 export type PasswordLoginRequestDto = {
-  username: string;
-  password: string;
-};
+  username: string
+  password: string
+}
 
 export type TokenLoginRequestDto = {
-  token: string;
-};
+  token: string
+}
 
 export type CreateUserRequestDto = {
-  username: string;
-  password: string;
-  email?: string | null;
-};
+  username: string
+  password: string
+  email?: string | null
+}
 
 export type AnonymousUserCreatedResponseDto = {
-  userId: string;
-};
+  userId: string
+}
 
 export type UserCreatedResponseDto = {
-  userId: string;
-};
+  userId: string
+}
 
 export type UsernameStatusResponseDto = {
-  username: string;
-  used: boolean;
-  valid: boolean;
-};
+  username: string
+  used: boolean
+  valid: boolean
+}
 
 export type EmailStatusResponseDto = {
-  email: string;
-  used: boolean;
-  valid: boolean;
-};
+  email: string
+  used: boolean
+  valid: boolean
+}
